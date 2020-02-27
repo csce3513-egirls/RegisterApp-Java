4f4d27fc1a1af8451fa341ead435d31f5363394f
samuel-witucki <samuel.witucki.p@gmail.com>
2020-02-27 16:27:21 -0600

    Changed ActiveEmployeeExistsQuery to be like other queries, currently implementing EmployeeSignInCommand

--- src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
+++ src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
@@ -5,29 +5,43 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
+
+import edu.uark.registerapp.commands.ResultCommandInterface;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.models.entities.EmployeeEntity;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
 
-public class ActiveEmployeeExistsQuery {
+public class ActiveEmployeeExistsQuery implements ResultCommandInterface<Boolean> {
 
     //EmployeeEntity employeeEntity = new EmployeeEntity();
     //EmployeeRepository employeeRepository = new EmployeeRepository(employeeEntity, java.util.UUID);
     
-    @Autowired
-    private EmployeeRepository employeeRepository;
 
-    public boolean activeEmployeeExistsQuery() {
 
-        //TODO: Check to make sure this is working correctly
+    @Override
+    public Boolean execute() {
+        final Boolean activeEmployee = this.employeeRepository.existsByIsActive(true);
+        if(activeEmployee != Boolean.TRUE) {
+            throw new NotFoundException("No active users found.");
+        }
+        return Boolean.TRUE;
+    }
+    
+/*
+    public boolean ActiveEmployeeExistsQuery() {
+
+        //TODO: Check to make sure this is working correctly (Professor says it works)
         final boolean activeEmployeeExists = employeeRepository.existsByIsActive(true);
 
         if(!activeEmployeeExists)
         {
-            throw new NotFoundException("No active users found.");
+            
             
         }
         return true;
     }
-
+*/
+    
+    @Autowired
+    private EmployeeRepository employeeRepository;
 }
--- src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -1,7 +1,71 @@
 package edu.uark.registerapp.commands.employees;
 
+import org.springframework.beans.factory.annotation.Autowired;
 
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
 
-public class EmployeeSignInCommand {
+import java.util.Arrays;
+import java.util.Optional;
+
+public class EmployeeSignInCommand implements VoidCommandInterface {
     
+    @Override
+    public void execute() {
+    // TODO Auto-generated method stub
+    
+    }
+
+
+    private EmployeeSignIn employeeSignIn;
+    private String sessionKey;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    
+    public EmployeeSignInCommand(EmployeeSignIn _employeeSignIn){
+        final String tempEmployeeId = _employeeSignIn.getEmployeeID();
+        final String tempEmployeePassword = _employeeSignIn.getPassword();
+
+        if(tempEmployeeId != null && !tempEmployeeId.isEmpty() && tempEmployeeId.matches("[0-9]+")){
+            employeeSignIn.setEmployeeID((tempEmployeeId));
+        }
+
+        if(tempEmployeePassword != null && !tempEmployeePassword.isEmpty()){
+            employeeSignIn.setPassword(tempEmployeePassword);
+        }
+    }
+
+    public boolean verifyEmployeeExists(){
+        final boolean employeeExists = employeeRepository.existsByEmployeeId(Integer.parseInt(employeeSignIn.getEmployeeID()));
+
+        if(employeeExists){
+            return true;
+        }
+        
+        return false;
+    }
+
+    public boolean verifyEmployeePassword(){
+        
+        
+        
+        //final boolean arePasswordsTheSame = Arrays.equals(employeeSignIn.getPassword().getBytes(),  employeeCheckPassword.get().getPassword()); 
+       
+        if(this.verifyEmployeeExists()){
+            final Optional<EmployeeEntity> employeeCheckPassword = employeeRepository.findByEmployeeId(Integer.parseInt(employeeSignIn.getEmployeeID()));
+            return Arrays.equals(employeeSignIn.getPassword().getBytes(),  employeeCheckPassword.get().getPassword());
+        }
+
+        return false;
+    }
+
+	
+
+    
+    
+
+
 }
