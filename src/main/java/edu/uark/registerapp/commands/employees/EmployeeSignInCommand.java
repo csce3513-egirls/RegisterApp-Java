d8c89a3ddeb4b434ad9b69e987be4a02ef4c99d4
samuel-witucki <samuel.witucki.p@gmail.com>
2020-02-27 20:27:18 -0600

    Created and wrote all code for both EmployeeSignInCommand and ActiveUserDeleteCommand, code finished for ActiveEmployeeExistsQuery. All code untested

--- src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -2,70 +2,100 @@ package edu.uark.registerapp.commands.employees;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.apache.commons.lang3.StringUtils;
+
 import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnauthorizedException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.models.api.Employee;
 import edu.uark.registerapp.models.api.EmployeeSignIn;
 import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
 
 import java.util.Arrays;
 import java.util.Optional;
 
+import org.springframework.transaction.annotation.Transactional;
+
 public class EmployeeSignInCommand implements VoidCommandInterface {
-    
+    @Transactional
     @Override
     public void execute() {
-    // TODO Auto-generated method stub
-    
+        this.validateProperties();
+
+        final Optional<EmployeeEntity> employeeEntity = this.employeeRepository.findByEmployeeId(Integer.parseInt(this.employeeSignIn.getEmployeeID()));
+       
+        if(!employeeEntity.isPresent()){
+            throw new NotFoundException("Employee");
+        }
+        
+        
+        if(!Arrays.equals(employeeSignIn.getPassword().getBytes(),  employeeEntity.get().getPassword())){
+            throw new UnauthorizedException();//TODO: is this the right exception to use?
+        }
+
+        final Optional<ActiveUserEntity> activeUserEntity = this.activeUserRepository.findByEmployeeId(employeeEntity.get().getId());
+
+        //is there an active user matching incoming user present?
+        if(activeUserEntity.isPresent()){
+            activeUserEntity.get().setSessionKey(this.sessionKey);
+            this.activeUserRepository.save(activeUserEntity.get()); //TODO: make sure return of employeeSignIn not required (see ProductUpdateCommand.java)
+        }else
+        {
+            activeUserEntity.get().setSessionKey(this.sessionKey);
+            activeUserEntity.get().setEmployeeId(employeeEntity.get().getId());
+            activeUserEntity.get().setName(employeeEntity.get().getFirstName() + employeeEntity.get().getLastName()); //TODO: make sure this is correctly formatted for name
+            activeUserEntity.get().setClassification(employeeEntity.get().getClassification());
+            this.activeUserRepository.save(activeUserEntity.get());//TODO: maybe move this from this and previous if statement outside, will run no matter what
+        }
+        
+
+        //TODO: return necessary?
     }
 
 
+
+    private void validateProperties(){
+        if(StringUtils.isBlank(this.employeeSignIn.getEmployeeID())){
+            throw new UnprocessableEntityException("EmployeeSignIn: blank name");
+        }
+
+        if(!this.employeeSignIn.getEmployeeID().matches("[0-9]+")){
+            throw new UnprocessableEntityException("EmployeeSignIn: non-numeric employee id");
+        }
+
+        if(StringUtils.isBlank(this.employeeSignIn.getPassword())){
+            throw new UnprocessableEntityException("EmployeeSignIn: blank password");
+        }
+    }
+
+    public EmployeeSignInCommand setSessionKey(final String _sessionKey){
+        this.sessionKey = _sessionKey;
+        return this;
+    }
+
+    public EmployeeSignInCommand setEmployeeSignIn(final EmployeeSignIn _employeeSignIn){
+        this.employeeSignIn = _employeeSignIn;
+        return this;
+    }
+
+
+    public EmployeeSignInCommand(final EmployeeSignIn _employeeSignIn, final String _sessionKey){
+
+
+    }
+
+    //properties
     private EmployeeSignIn employeeSignIn;
     private String sessionKey;
 
     @Autowired
     private EmployeeRepository employeeRepository;
-    
-    public EmployeeSignInCommand(EmployeeSignIn _employeeSignIn){
-        final String tempEmployeeId = _employeeSignIn.getEmployeeID();
-        final String tempEmployeePassword = _employeeSignIn.getPassword();
-
-        if(tempEmployeeId != null && !tempEmployeeId.isEmpty() && tempEmployeeId.matches("[0-9]+")){
-            employeeSignIn.setEmployeeID((tempEmployeeId));
-        }
-
-        if(tempEmployeePassword != null && !tempEmployeePassword.isEmpty()){
-            employeeSignIn.setPassword(tempEmployeePassword);
-        }
-    }
-
-    public boolean verifyEmployeeExists(){
-        final boolean employeeExists = employeeRepository.existsByEmployeeId(Integer.parseInt(employeeSignIn.getEmployeeID()));
-
-        if(employeeExists){
-            return true;
-        }
-        
-        return false;
-    }
-
-    public boolean verifyEmployeePassword(){
-        
-        
-        
-        //final boolean arePasswordsTheSame = Arrays.equals(employeeSignIn.getPassword().getBytes(),  employeeCheckPassword.get().getPassword()); 
-       
-        if(this.verifyEmployeeExists()){
-            final Optional<EmployeeEntity> employeeCheckPassword = employeeRepository.findByEmployeeId(Integer.parseInt(employeeSignIn.getEmployeeID()));
-            return Arrays.equals(employeeSignIn.getPassword().getBytes(),  employeeCheckPassword.get().getPassword());
-        }
-
-        return false;
-    }
-
-	
-
+    private ActiveUserRepository activeUserRepository;
     
     
-
 
 }
